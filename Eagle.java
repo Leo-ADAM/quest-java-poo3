e8ead837683df6118f5d63b57d2828ffc02d22d4
Leo-ADAM <leo.adam045@gmail.com>
2019-10-01 17:30:00 +0200

    Quete POO

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly{
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,35 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+    public void takeOff() {
+	if(!this.flying && this.altitude == 0){
+		this.flying = true;
+		System.out.println(this.getName() + " takes off in the sky.");
+	}
+    }
+	
+    public int ascend(int meters) {
+	if (this.flying) {
+		this.altitude = Math.max(this.altitude + meters, 120);
+		System.out.println(this.getName() + " flies upward, altitude : " + this.altitude);
+	}
+	return this.altitude;
+    }	
+	
+    public int descend(int meters) {
+	if (this.flying){
+		this.altitude = Math.min(this.altitude - meters, 140);
+		System.out.println(this.getName() + " flies downward, altitude : " + this.altitude);
+	}
+	return this.altitude;
+    }	
+    public void land() {
+	if (this.flying && this.altitude <= 1){
+		System.out.println(this.getName() + " lands on the ground.");
+	} else {
+		System.out.println(this.getName() + " is too high, it can't lands.");
+	}
+
     }
 }
